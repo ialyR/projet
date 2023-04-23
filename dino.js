cb8be5604d18c26784c146e36833ad52b710bb98
arian <arian@DESKTOP-USIQFAJ>
2023-04-23 23:34:27 +0200

    mise en place Element

--- dino.js
+++ dino.js
@@ -1,2 +1,15 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
+
+// Variables
+let dino;
+
+
+// Prototype Element
+function Element (x, y, w, h, c) {
+	this.xpos=x;
+	this.ypos=y;
+	this.width=w;
+	this.height=h;
+	this.color=c;
+}
