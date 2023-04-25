f0a634487834a5192cddb2bdfc6818156f80be94
arian <arian@DESKTOP-USIQFAJ>
2023-04-25 21:03:23 +0200

    MAJ de la gravité, mise en place du saut

--- dino.js
+++ dino.js
@@ -5,7 +5,18 @@ const ctx = canvas.getContext('2d');
 let dino;
 let vitesse;
 let gravite;
+let keys={};
+let nSaut=0;
+let obstacles=[];
 
+
+
+document.addEventListener('keydown', function (evt) {
+    keys[evt.code] = true;
+  });
+  document.addEventListener('keyup', function (evt) {
+    keys[evt.code] = false;
+});
 
 // Prototype Element
 function Element (x, y, w, h, c) {
@@ -15,21 +26,38 @@ function Element (x, y, w, h, c) {
 	this.height=h;
 	this.color=c;
 
-    
+    this.dy=0;
+    this.fSaut=15;
+    this.grounded = false;
 }
     
 Element.prototype.Animer = function () {
-    this.Dessiner();
+    
     
     // ligne du sol
     ctx.moveTo(0,canvas.height);
     ctx.lineTo(canvas.width,canvas.height);
     ctx.stroke();
 
-    //animation de la gravité 
-    if (this.y+this.h<canvas.height){
-
+    //animation des sauts
+    if (keys['Space']) {
+        this.Sauter();
+      } else {
+        this.nSaut = 0;
     }
+    this.ypos += this.dy;
+
+    //animation de la gravité 
+    if (this.ypos+this.height<canvas.height){
+        this.dy +=gravite;
+        this.grounded=false;
+    }
+    else{
+        this.dy = 0;
+        this.grounded = true;
+        this.ypos = canvas.height - this.height;
+    }
+    this.Dessiner();
 }
     
 Element.prototype.Dessiner = function() {
@@ -41,7 +69,37 @@ Element.prototype.Dessiner = function() {
 }
 
 Element.prototype.Sauter = function(){
+    if (this.grounded && this.nSaut == 0) {
+        this.nSaut = 1;
+        this.dy = -this.fSaut;
+      } else if (this.nSaut > 0 && this.nSaut < 15) {
+        this.nSaut++;
+        this.dy = -this.fSaut - (this.nSaut / 50);
+      }
+  
+}
 
+function Obstacle(x, y, w, h, c){
+    this.x1 = x;
+    this.y1 = y;
+    this.w1 = w;
+    this.h1 = h;
+    this.c1 = c;
+
+    this.dx=-vitesse;
+}
+
+Obstacle.prototype.Update=function(){
+    this.x1 += this.dx;
+    this.Dessiner();
+    this.dx = -vitesse;
+}
+
+Obstacle.prototype.Dessiner=function(){
+    ctx.beginPath();
+    ctx.fillStyle = this.c1;
+    ctx.fillRect(this.x1, this.y1, this.w1, this.h1);
+    ctx.closePath();
 }
 
 function Lancer() {
@@ -54,6 +112,12 @@ function Lancer() {
 	requestAnimationFrame(Update);
 }
 
+function Update() {
+	requestAnimationFrame(Update);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	dino.Animer();
+}
 
+Lancer();
 
 
